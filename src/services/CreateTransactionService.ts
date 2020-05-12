c4f119c2b27e8bc8f1748ce2b9e93ab0facc69b9
Vitoria Lui <vitorialui@hotmail.com>
2020-05-12 00:48:39 -0300

    Create and list transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, type, value}: Request): Transaction {
+
+    if(!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enought balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value
+    });
+
+    return transaction;
   }
 }
 
